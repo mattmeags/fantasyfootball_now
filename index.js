26c1b7887b83d8ca87892b66de50fb125876a69c
Matt Meagher <mattmeags@Matts-iMac.local>
2019-09-05 10:23:00 -0500

    global styles: typographies, grid, cards, buttons, colors etc

--- index.js
+++ index.js
@@ -56,5 +56,5 @@ app.post('/loadPlayer', (req, res) => {
 
 const port = process.env.PORT || 4000;
 app.listen(port, ()=> {
-    console.log('listening on ${port}');
+    console.log('listening on ' + port);
 });
