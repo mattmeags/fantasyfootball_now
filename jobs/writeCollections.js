0ae7e1ede3c4f5d68614ba276d5ffbdf26345970
Matt Meagher <>
2019-12-03 14:23:56 -0600

    added mongo write job and boilerplater mongodb code to be used throughout app

---
+++ jobs/writeCollections.js
@@ -0,0 +1,65 @@
+const mongoClient = require('../mongoClient');
+const globals = require('../models/global');
+
+function init(callback) {
+    mongoClient.createConnection(callback);
+}
+
+async function addCollections() {
+    const db = mongoClient.getDb();
+    const defense = require('../data_files/json/all/defense');
+    const offense = require('../data_files/json/all/offense');
+    const passDefense = require('../data_files/json/all/passDefense');
+    const rushDefense = require('../data_files/json/all/rushDefense');
+    const rushOffense = require('../data_files/json/all/rushOffense');
+    const passOffense = require('../data_files/json/all/passOffense');
+    let allCollection = [];
+    allCollection.push(
+        {
+            defense: defense,
+            offense: offense,
+            passDefense: passDefense,
+            rushDefense: rushDefense,
+            rushOffense, rushOffense,
+            passOffense: passOffense
+        }
+    );
+    db.collection('all').drop()
+    db.collection('all').insertMany(allCollection, function(err, res) {
+        if (err) {
+            console.log('line 23');
+            throw (err);
+          }
+    });
+
+    let addCollectionPromises = globals.teams.map(team => {
+        return new Promise(function(resolve, reject) {
+            const teamRushRecCollection = require('../data_files/json/' + team + '/rushRec');
+            const teamPassCollection = require('../data_files/json/' + team + '/passing');
+      
+            let teamCollection = [];
+            teamCollection.push({
+                rushRec: teamRushRecCollection,
+                passing: teamPassCollection
+            });
+            new Promise(function(resolve, reject) {
+                db.collection(team).drop();
+                resolve();
+            }).then(() => {
+                db.collection(team).insertMany(teamCollection, function(err, res) {
+                    if (err) {
+                        throw (err);
+                    }
+                    resolve(res);
+                });
+            });
+        }); 
+    });
+
+    Promise.all(addCollectionPromises).then(() => {
+        console.log('done');
+        mongoClient.closeConnection();
+    });
+}
+
+init(addCollections);
---
+++ mongoClient.js
@@ -0,0 +1,31 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://ffNow:Kimball1890@@cluster0-ad3fi.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+let _db, _client;
+
+module.exports = {
+  createConnection: function(callback) {
+    client.connect((err, db) => {
+    
+      // perform actions on the collection object
+      if (err) {
+        throw err;
+      }
+      console.log(db);
+      //const collection = client.db("ffNow").collection("test");
+      _client = db;
+      _db = db.db("ffNow");
+      console.log(callback);
+      if (callback) {
+        return callback();
+      }
+    });  
+  },
+  getDb: function() {
+    return _db;
+  },
+  closeConnection: function() {
+    _client.close();   
+  }
+}
